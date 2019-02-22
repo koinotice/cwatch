310289b8cdd999c3e41d08ac22ae2af63095156d
koinotice <koinotice@gmail.com>
2019-02-22 23:42:15 +0800

    new contract

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var gethIPCPath = "https://rinkeby.infura.io/v3/26b9e69bffe84bafbd0e16cbad56365f";
 
-const Web3=require("web3")
-const {address,hashabi}=require("./hashDice")
+const Web3 = require("web3")
+const {address, hashabi} = require("./hashDice")
 var RINKEBY_WSS = 'wss://rinkeby.infura.io/ws';
 var provider = new Web3.providers.WebsocketProvider(RINKEBY_WSS);
 var web3 = new Web3(provider);
+
+const nats = require('nats').connect("nats://admin:Zheli123@71an.com:4222");
 
 provider.on('error', e => {
     console.error('WS Infura Error', e);
@@ -35,9 +37,9 @@ const options = {
     toBlock: 'latest'
 }
 console.log('Start programs !');
-listening();
+
 // Subscribe to Transfer events matching filter criteria
-async function listening() {
+async function rooms() {
     const events = await myContract.getPastEvents(
         'RoomOpened',
         {
@@ -48,10 +50,93 @@ async function listening() {
 
     await Promise.all(events.map(async event => {
 
-       // nats.publish(this.contractAddress, JSON.stringify( event));
-        console.log(event)
+        nats.publish(address, JSON.stringify(event));
+        //console.log(event)
     }))
-    
-
-
 }
+
+async function orders() {
+    const events = await myContract.getPastEvents(
+        'NewBetOrder',
+        {
+            fromBlock: 0,
+            toBlock: "latest"
+        }
+    )
+
+    await Promise.all(events.map(async event => {
+
+        nats.publish(address, JSON.stringify(event));
+        //console.log(event)
+    }))
+}
+
+async function allEvents() {
+    const events = await myContract.getPastEvents(
+        'NewBetOrder',
+        {
+            fromBlock: 0,
+            toBlock: "latest"
+        }
+    )
+
+    await Promise.all(events.map(async event => {
+
+        nats.publish(address, JSON.stringify(event));
+
+    }))
+}
+
+let ClastBlock = 0
+
+async function checkForEvents() {
+    const nextBlock = ClastBlock + 1
+    const lastBlock = await new Promise(
+        (reject, resolve) =>
+            web3.eth.getBlockNumber((result, err) => {
+                if (err) reject(err)
+                resolve(result)
+            })
+    )
+    console.log(nextBlock, lastBlock)
+    // check for new bid events
+    if (nextBlock <= lastBlock) {
+        console.log("0000")
+        const events = await myContract.getPastEvents(
+            'allEvents',
+            {
+                fromBlock: nextBlock,
+                toBlock: lastBlock
+            }
+        )
+
+        await Promise.all(events.map(async event => {
+            console.log(event)
+            nats.publish(address, JSON.stringify(event));
+        }))
+       // await Contract.update({address: this.contractAddress}, {$set: {lastBlock: lastBlock}}, {})
+
+
+        ClastBlock = lastBlock
+    }
+}
+
+
+
+async function main() {
+    await rooms()
+    await orders()
+
+    setInterval(async () => {
+        try {
+
+            await checkForEvents()
+
+        } catch (err) {
+            console.error(err)
+
+        }
+    }, 15 * 1000) // every 15 seconds check for new events
+}
+
+main();
